f9fc8ae3a1636cae193b175b6dccbe35ad75afe4
enrocha0312 <enrocha0312@gmail.com>
2023-12-08 09:47:33 -0300

    entity e repository

---
+++ src/main/java/com/eluminivrbeneficios/eduardondarocha/entities/Cartao.java
@@ -0,0 +1,22 @@
+package com.eluminivrbeneficios.eduardondarocha.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cartoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cartao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @NotEmpty
+    private String numeroCartao;
+    @NotEmpty
+    private String senha;
+}
---
+++ src/main/java/com/eluminivrbeneficios/eduardondarocha/repositories/CartaoRepository.java
@@ -0,0 +1,7 @@
+package com.eluminivrbeneficios.eduardondarocha.repositories;
+
+import com.eluminivrbeneficios.eduardondarocha.entities.Cartao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartaoRepository extends JpaRepository<Cartao, Integer> {
+}
