fdfa8c150a8fd27a9f23fb0088e2e10d49f17621
enrocha0312 <enrocha0312@gmail.com>
2023-12-08 09:34:03 -0300

    iniciando projeto

---
+++ src/test/java/com/eluminivrbeneficios/eduardondarocha/EduardondarochaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eluminivrbeneficios.eduardondarocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduardondarochaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
